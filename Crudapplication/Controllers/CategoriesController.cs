5dfe064582581bb78e335e2807a22c01ec03b49b
manojtharindu11 <thilakarathnawatm.21@uom.lk>
2024-01-17 23:07:50 +0530

    Add project files.

---
+++ Crudapplication/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Crudapplication.Models;
+
+namespace Crudapplication.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            var category = new Category { categoryId = id.HasValue?id.Value:0 };
+
+            return View(category);
+        }
+    }
+}
---
+++ Crudapplication/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace Crudapplication.Models
+{
+    public class Category
+    {
+        public int categoryId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+    }
+}

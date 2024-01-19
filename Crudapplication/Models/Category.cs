fe63ace0ba66e75e9262d2d1f0dc4f960e22cbf3
manojtharindu11 <thilakarathnawatm.21@uom.lk>
2024-01-19 17:28:51 +0530

    Add validation

--- Crudapplication/Models/Category.cs
+++ Crudapplication/Models/Category.cs
@@ -1,9 +1,11 @@
-﻿namespace Crudapplication.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Crudapplication.Models
 {
     public class Category
     {
         public int CategoryId { get; set; }
-
+        [Required]
         public string Name { get; set; } = string.Empty;
 
         public string Description { get; set; } = string.Empty;
